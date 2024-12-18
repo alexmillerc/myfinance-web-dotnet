3350d1694d933cc1b1a1702804f33f9942b79f0e
ALEX MILLER CARVALHO <67715455+alexmillerc@users.noreply.github.com>
2024-12-18 09:07:46 -0300

    Construção Base Aula Final

--- myfinance-web-netcore/Controllers/PlanoContaController.cs
+++ myfinance-web-netcore/Controllers/PlanoContaController.cs
@@ -11,35 +11,56 @@ namespace myfinance_web_netcore.Controllers;
     public class PlanoContaController : Controller
     {
         private readonly IPlanoContaService _planoContaService;
-        
-        public PlanoContaController(IPlanoContaService planoContaService) 
+        public PlanoContaController(IPlanoContaService planoContaService)
         {
             _planoContaService = planoContaService;
         }
-       
         [Route("Index")]
         public IActionResult Index()
         {
             ViewBag.Lista = _planoContaService.ListarRegistros();
             return View();
         }
-
-        [HttpPost]
         [HttpGet]
+        [HttpPost]
         [Route("Cadastro")]
-        public IActionResult Cadastro(PlanoContaModel? model)
+        [Route("Cadastro/{id}")]
+        public IActionResult Cadastro(PlanoContaModel? planoContaModel, int? id)
         {
-            if (model != null && ModelState.IsValid)
+            if(id != null && !ModelState.IsValid)
+            {
+                var registro = _planoContaService.RetornarRegistro((int)id);
+
+                var model = new PlanoContaModel()
+                {
+                    Id = registro.Id,
+                    Nome = registro.Nome,
+                    Tipo = registro.Tipo
+                };
+
+                return View(model);
+            }
+            else if(planoContaModel != null && ModelState.IsValid)
             {
                 var planoConta = new PlanoConta
                 {
-                    Id = model.Id,
-                    Nome = model.Nome,
-                    Tipo = model.Tipo
+                    Id = planoContaModel.Id,
+                    Nome = planoContaModel.Nome,
+                    Tipo = planoContaModel.Tipo
                 };
                 _planoContaService.Salvar(planoConta);
+                return RedirectToAction("Index");
             }
-
-            return View();
+            else
+            {
+                return View();
+            }
+        }
+        [HttpGet]
+        [Route("Excluir/{id}")]
+        public IActionResult Excluir(int id)
+        {
+            _planoContaService.Excluir(id);
+            return RedirectToAction("Index");
         }
     }
--- myfinance-web-netcore/Services/PlanoContaService.cs
+++ myfinance-web-netcore/Services/PlanoContaService.cs
@@ -6,7 +6,6 @@ namespace myfinance_web_netcore.Services
     public class PlanoContaService : IPlanoContaService
     {
         private MyFinanceDbContext _myFinanceDbContext;
-
         public PlanoContaService(MyFinanceDbContext myFinanceDbContext)
         {
             _myFinanceDbContext = myFinanceDbContext;
@@ -14,7 +13,10 @@ namespace myfinance_web_netcore.Services
 
         public void Excluir(int id)
         {
-            throw new NotImplementedException();
+            var item = RetornarRegistro(id);
+            _myFinanceDbContext.Attach(item);
+            _myFinanceDbContext.Remove(item);
+            _myFinanceDbContext.SaveChanges();
         }
 
         public List<PlanoConta> ListarRegistros()
@@ -25,14 +27,17 @@ namespace myfinance_web_netcore.Services
 
         public PlanoConta RetornarRegistro(int id)
         {
-            return new PlanoConta();
+            var item = _myFinanceDbContext.PlanoConta.Where(x => x.Id == id).First();
+            return item;
         }
 
         public void Salvar(PlanoConta item)
         {
             var dbSet = _myFinanceDbContext.PlanoConta;
             if (item.Id == null)
+            {
                 dbSet.Add(item);
+            }
             else
             {
                 dbSet.Attach(item);
